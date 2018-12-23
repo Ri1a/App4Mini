323d4ff8a20de57a14996d2150f72efd96f91dbe
RiccardoLandolfo <ricky.landolfo@gmx.ch>
2018-12-23 22:48:28 +0100

    Initial commit

---
+++ src/com/ims/idpa/app/IndexForm.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ims.idpa.app;
+
+/**
+ * GUI builder created Form
+ *
+ * @author ricky
+ */
+public class IndexForm extends com.codename1.ui.Form {
+
+    public IndexForm() {
+        this(com.codename1.ui.util.Resources.getGlobalResources());
+    }
+    
+    public IndexForm(com.codename1.ui.util.Resources resourceObjectInstance) {
+        initGuiBuilderComponents(resourceObjectInstance);
+    }
+
+//-- DON'T EDIT BELOW THIS LINE!!!
+    private void initGuiBuilderComponents(com.codename1.ui.util.Resources resourceObjectInstance) {
+    }
+//-- DON'T EDIT ABOVE THIS LINE!!!
+}
