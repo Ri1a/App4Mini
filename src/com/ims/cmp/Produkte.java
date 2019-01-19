6ec32eb25067289f1e1f0a65141e21897082733e
RiccardoLandolfo <ricky.landolfo@gmx.ch>
2019-01-19 12:26:55 +0100

    get src from image-array

--- src/com/ims/cmp/Produkte.java
+++ src/com/ims/cmp/Produkte.java
@@ -11,19 +11,19 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
  *
- * @author Riccardo, Joel, Yanick, Alain
- * Version: 1.0.0
+ * @author Riccardo, Joel, Yanick, Alain Version: 1.0.0
  */
 public class Produkte {
 
     ArrayList<String> productsArr = new ArrayList<>();
-    ArrayList<String> productImages = new ArrayList<>();
     ArrayList<String> productStock = new ArrayList<>();
     ArrayList<String> productDateCreated = new ArrayList<>();
+    ArrayList<String> productImages = new ArrayList<>();
 
     public void getProdukte() throws JSONException {
 
@@ -36,19 +36,21 @@ public class Produkte {
             r.setUrl("https://" + tokens.getShop_name_token() + ".ch/wp-json/wc/v3/products?consumer_key=" + tokens.getConsumer_key_token() + "&consumer_secret=" + tokens.getSecret_key_token());
             NetworkManager.getInstance().addToQueueAndWait(r);
             Map<String, Object> result = new JSONParser().parseJSON(new InputStreamReader(new ByteArrayInputStream(r.getResponseData()), "UTF-8"));
-
             //JSON Filter     
             ArrayList<Map<String, String>> myList = (ArrayList<Map<String, String>>) result.get("root");
             for (int i = 0; i < myList.size(); i++) {
-                
+
                 Map<String, String> dtls = myList.get(i);
                 productsArr.add(dtls.get("name"));
-                productImages.add(dtls.get("images"));
                 productStock.add(dtls.get("stock_status"));
                 productDateCreated.add(dtls.get("date_created"));
-                //TODO: Filter out image-source
             }
-            
+            //Filter out the source of the image. Code from: https://stackoverflow.com/questions/54252836/java-extract-index-src-from-multiarray
+            for (Map<String, Object> element : (List<Map<String, Object>>) result.get("root")) {
+                for (Map<String, Object> image : (List<Map<String, Object>>) element.get("images")) {
+                    productImages.add((String) image.get("src"));
+                }
+            }
         } catch (IOException e) {
             loginForm.show();
             Dialog.show("Produkte konnten nicht angezeigt werden.", "", "OK", null);
@@ -59,16 +61,15 @@ public class Produkte {
         return productsArr;
     }
 
-    public ArrayList<String> getProductImages() {
-        return productImages;
-    }
-
     public ArrayList<String> getProductStock() {
         return productStock;
     }
-    
+
     public ArrayList<String> getProductDateCreated() {
         return productDateCreated;
     }
-    
+
+    public ArrayList<String> getProductImages() {
+        return productImages;
+    }
 }
--- src/com/ims/idpa/app/ProdukteForm.java
+++ src/com/ims/idpa/app/ProdukteForm.java
@@ -7,6 +7,7 @@ import com.codename1.ui.Button;
 import com.codename1.ui.Component;
 import com.codename1.ui.Container;
 import com.codename1.ui.FontImage;
+import com.codename1.ui.Image;
 import com.codename1.ui.Label;
 import com.codename1.ui.Toolbar;
 import com.codename1.ui.events.ActionEvent;
@@ -18,8 +19,7 @@ import java.util.List;
 
 /**
  *
- * @author Riccardo, Joel, Yanick, Alain
- * Version: 1.0.0
+ * @author Riccardo, Joel, Yanick, Alain Version: 1.0.0
  */
 public class ProdukteForm extends com.codename1.ui.Form {
 
@@ -31,99 +31,100 @@ public class ProdukteForm extends com.codename1.ui.Form {
 
     public ProdukteForm(com.codename1.ui.util.Resources resourceObjectInstance) {
 
-            //Menu
-            Toolbar tb = this.getToolbar();
-            Container topBar = BorderLayout.east(new Label(""));
-            topBar.add(BorderLayout.SOUTH, new Label(""));
-            topBar.setUIID("SideCommand");
-            tb.addComponentToSideMenu(topBar);
+        //Menu
+        Toolbar tb = this.getToolbar();
+        Container topBar = BorderLayout.east(new Label(""));
+        topBar.add(BorderLayout.SOUTH, new Label(""));
+        topBar.setUIID("SideCommand");
+        tb.addComponentToSideMenu(topBar);
 
-            tb.addMaterialCommandToSideMenu("Home", FontImage.MATERIAL_HOME, (ActionListener) (ActionEvent evt) ->  {
-                    new IndexForm().show();
-            });
-            tb.addMaterialCommandToSideMenu("Produkte", FontImage.MATERIAL_WEB, (ActionListener) (ActionEvent evt) ->  {
-                    new ProdukteForm().show();
-            });
-            tb.addMaterialCommandToSideMenu("Bestellungen", FontImage.MATERIAL_SHOPPING_CART, (ActionListener) (ActionEvent evt) ->   {
-                    new BestellungenForm().show();
-            });
-            tb.addMaterialCommandToSideMenu("Logout", FontImage.MATERIAL_POWER_SETTINGS_NEW, (ActionListener) (ActionEvent evt) -> {
-                Preferences.clearAll();
-                new LoginForm().show();
-            });
+        tb.addMaterialCommandToSideMenu("Home", FontImage.MATERIAL_HOME, (ActionListener) (ActionEvent evt) -> {
+            new IndexForm().show();
+        });
+        tb.addMaterialCommandToSideMenu("Produkte", FontImage.MATERIAL_WEB, (ActionListener) (ActionEvent evt) -> {
+            new ProdukteForm().show();
+        });
+        tb.addMaterialCommandToSideMenu("Bestellungen", FontImage.MATERIAL_SHOPPING_CART, (ActionListener) (ActionEvent evt) -> {
+            new BestellungenForm().show();
+        });
+        tb.addMaterialCommandToSideMenu("Logout", FontImage.MATERIAL_POWER_SETTINGS_NEW, (ActionListener) (ActionEvent evt) -> {
+            Preferences.clearAll();
+            new LoginForm().show();
+        });
 
-            Produkte produkte = new Produkte();
+        Produkte produkte = new Produkte();
         try {
             produkte.getProdukte();
         } catch (JSONException ex) {
         }
 
-            //Show products in app
-            //TODO: Scrolling not working
-            Container conProducts = new Container(new BoxLayout(BoxLayout.Y_AXIS));
-            conProducts.setScrollableY(true);
-            conProducts.setUIID("conProducts");
+        //Show products in app
+        //TODO: Scrolling not working
+        Container conProducts = new Container(BoxLayout.y());
+        conProducts.setScrollableY(true);
+        conProducts.setUIID("conProducts");
 
-            //Get elements from array
-            for (int i = 0; i < produkte.getProductsArr().size(); i++) {
-                String product = produkte.getProductsArr().get(i);
-                String stock = produkte.getProductStock().get(i);
-                String dateCreated = produkte.getProductDateCreated().get(i);
+        //Get elements from array
+        for (int i = 0; i < produkte.getProductsArr().size(); i++) {
+            String product = produkte.getProductsArr().get(i);
+            String stock = produkte.getProductStock().get(i);
+            String dateCreated = produkte.getProductDateCreated().get(i);
+            //TODO: String cannot be converted to Image
+            //Image imgSrc = produkte.getProductImages().get(i);
 
-                //All orders
-                MultiButton mbProducts = new MultiButton(product);
-
-                switch (stock) {
-                    case "instock":
-                        //Change the second line to color green
-                        setLine2Color(mbProducts, 0x4BA42F);
-                        mbProducts.setTextLine2("Auf Lager");
-                        break;
-                    case "outofstock":
-                        //Change the second line to color yellow
-                        setLine2Color(mbProducts, 0x980101);
-                        mbProducts.setTextLine2("Nicht auf Lager");
-                        break;
-                    default:
-                        mbProducts.setTextLine2(stock);
-                        break;
-                }
-
-                //Produkte in Detail (Form)
-                ProdukteDetailForm produkteDetailForm = new ProdukteDetailForm();
-                //Backbutton
-                Button btnBack = new Button("Zurück");
-                btnBack.addActionListener(b -> {
-                    new ProdukteForm().show();
-                });
-
-                //ProductDetails
-                Container conProdukteDetail = new Container(BoxLayout.y());
-                conProdukteDetail.add(btnBack);
-                conProdukteDetail.add(new Label("Name: " + product));
-                switch (stock) {
-                    case "instock":
-                        stock = "Auf Lager";
-                        break;
-                    case "outofstock":
-                        stock = "Nicht auf Lager";
-                        break;
-                }
-                conProdukteDetail.add(new Label("Status: " + stock));
-                conProdukteDetail.add(new Label("Erstellt am: " + dateCreated));
-                produkteDetailForm.add(conProdukteDetail);
-                //Show ProductDetails (all Products)
-                mbProducts.addActionListener(e -> {
-                    produkteDetailForm.show();
-                });
-                conProducts.add(mbProducts);
+            //All orders
+            MultiButton mbProducts = new MultiButton(product);
+            //mbProducts.setIcon(imgSrc); -> Line 72
+            switch (stock) {
+                case "instock":
+                    //Change the second line to color green
+                    setLine2Color(mbProducts, 0x4BA42F);
+                    mbProducts.setTextLine2("Auf Lager");
+                    break;
+                case "outofstock":
+                    //Change the second line to color yellow
+                    setLine2Color(mbProducts, 0x980101);
+                    mbProducts.setTextLine2("Nicht auf Lager");
+                    break;
+                default:
+                    mbProducts.setTextLine2(stock);
+                    break;
             }
-            this.add(conProducts);
-            initGuiBuilderComponents(resourceObjectInstance);
-        
+
+            //Produkte in Detail (Form)
+            ProdukteDetailForm produkteDetailForm = new ProdukteDetailForm();
+            //Backbutton
+            Button btnBack = new Button("Zurück");
+            btnBack.addActionListener(b -> {
+                new ProdukteForm().show();
+            });
+
+            //ProductDetails
+            Container conProdukteDetail = new Container(BoxLayout.y());
+            conProdukteDetail.add(btnBack);
+            conProdukteDetail.add(new Label("Name: " + product));
+            switch (stock) {
+                case "instock":
+                    stock = "Auf Lager";
+                    break;
+                case "outofstock":
+                    stock = "Nicht auf Lager";
+                    break;
+            }
+            conProdukteDetail.add(new Label("Status: " + stock));
+            conProdukteDetail.add(new Label("Erstellt am: " + dateCreated));
+            produkteDetailForm.add(conProdukteDetail);
+            //Show ProductDetails (all Products)
+            mbProducts.addActionListener(e -> {
+                produkteDetailForm.show();
+            });
+            conProducts.add(mbProducts);
+        }
+        this.add(conProducts);
+        initGuiBuilderComponents(resourceObjectInstance);
+
     }
     //-- DON'T EDIT BELOW THIS LINE!!!
-
 
 // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initGuiBuilderComponents(com.codename1.ui.util.Resources resourceObjectInstance) {
@@ -131,7 +132,7 @@ public class ProdukteForm extends com.codename1.ui.Form {
         setInlineStylesTheme(resourceObjectInstance);
         setScrollableX(true);
         setScrollableY(true);
-                setInlineStylesTheme(resourceObjectInstance);
+        setInlineStylesTheme(resourceObjectInstance);
         setTitle("Produkte");
         setName("ProdukteForm");
     }// </editor-fold>
